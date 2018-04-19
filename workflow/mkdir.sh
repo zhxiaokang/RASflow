164d10cf482e96644f1fb2689695d0142d8d1a2c
zhxiaokang <zhxiaokang@gmail.com>
2018-04-19 16:17:44 +0200

    workflow for alignment and feature count

---
+++ workflow/mkdir.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+DATAPATH='../../data/RNA-Seq-analysis'
+OUTPUTPATH='../../data/RNA-Seq-analysis/output'
+# mkdir $DATAPATH/reads
+mkdir $OUTPUTPATH $OUTPUTPATH/reads $OUTPUTPATH/indexes $OUTPUTPATH/samFile $OUTPUTPATH/bamFile $OUTPUTPATH/benchmarks $OUTPUTPATH/countFile
