5f632a194ed33aeb44bccb9e124df0d5664b5338
zhxiaokang <zhxiaokang@gmail.com>
2018-05-10 22:06:11 +0200

    create folders for output from edgeR and DESeq2

--- workflow/mkdir.sh
+++ workflow/mkdir.sh
@@ -3,4 +3,4 @@
 DATAPATH='/export/jonassenfs/xiaokangz/project/data/RNA-Seq-analysis'
 OUTPUTPATH='/export/jonassenfs/xiaokangz/project/data/RNA-Seq-analysis/output'
 mkdir $DATAPATH/reads
-mkdir $OUTPUTPATH $OUTPUTPATH/reads $OUTPUTPATH/indexes $OUTPUTPATH/samFile $OUTPUTPATH/bamFile $OUTPUTPATH/benchmarks $OUTPUTPATH/countFile
+mkdir $OUTPUTPATH $OUTPUTPATH/reads $OUTPUTPATH/indexes $OUTPUTPATH/samFile $OUTPUTPATH/bamFile $OUTPUTPATH/benchmarks $OUTPUTPATH/countFile $OUTPUTPATH/DEA $OUTPUTPATH/DEA/edger $OUTPUTPATH/DEA/deseq2
