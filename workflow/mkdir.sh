06b58e3ab0e92a840be818e4382bf5e7a0921238
xkzhang <zhxiaokang@gmail.com>
2019-05-08 16:42:37 +0200

    delete the script to make directories
    Snakemake will generate folders by itself

--- workflow/mkdir.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DATAPATH='/export/jonassenfs/xiaokangz/project/data/RNA-Seq-analysis'
-OUTPUTPATH='/export/jonassenfs/xiaokangz/project/data/RNA-Seq-analysis/output'
-mkdir $DATAPATH/reads
-mkdir $OUTPUTPATH $OUTPUTPATH/reads $OUTPUTPATH/indexes $OUTPUTPATH/samFile $OUTPUTPATH/bamFile $OUTPUTPATH/benchmarks $OUTPUTPATH/countFile $OUTPUTPATH/DEA $OUTPUTPATH/DEA/edger $OUTPUTPATH/DEA/deseq2
