9b7276b594cb531d84eefdbed11088b83052127c
xkzhang <zhxiaokang@gmail.com>
2019-07-04 19:02:40 +0200

    The script to do DEA with edgeR
    It contains both pair and non-pair test

---
+++ scripts/dea.R
@@ -0,0 +1,111 @@
+library(yaml)
+library(edgeR)
+
+# Use edgeR to do DEA
+
+# load the config file
+yaml.file <- yaml.load_file('config_dea.yaml')
+
+# extract the information from the yaml file
+comparison <- yaml.file$COMPARISON
+controls <- comparison$control  # all groups used as control
+treats <- comparison$treat  # all groups used as treat, should correspond to control
+filter.need <- yaml.file$FILTER$yesOrNo
+cpm.threshold <- yaml.file$FILTER$cpm
+pair.test <- yaml.file$PAIR
+
+num.control <- length(controls)  # number of comparisons that the user wants to do
+num.treat <- length(treats)  # should equals to num.control
+
+if (num.control != num.treat) {
+  message("Error: Control groups don't mathch with treat groups!")
+  message("Please check config_dea.yaml")
+  quit(save = 'no')
+}
+
+num.comparison <- num.control
+
+DEA <- function(control, treat) {
+  count.control <- read.table(paste(control, 'count.tsv', sep = '_'), header = TRUE, row.names = 1)
+  count.treat <- read.table(paste(treat, 'count.tsv', sep = '_'), header = TRUE, row.names = 1)
+  count.table <- cbind(count.control, count.treat)  # merge the control and treat tables together
+  
+  # number of samples in control and treat groups (should be the same if it's a pair test)
+  num.sample.control <- ncol(count.control)
+  num.sample.treat <- ncol(count.treat)
+  
+  # save gene list in gene.list for extracting gene names later
+  gene.list <- rownames(count.table)
+  
+  # get the sample id
+  samples <- colnames(count.table)
+  
+  # Put the data into a DGEList object
+  y <- DGEList(counts = count.table, genes = gene.list)
+  
+  # Filtering
+  if (filter.need) {
+    countsPerMillion <- cpm(y)
+    countCheck <- countsPerMillion > cpm.threshold
+    keep <- which(rowSums(countCheck) >= 2)
+    y <- y[keep, ]
+  }
+  
+  # Normalization
+  y <- calcNormFactors(y, method="TMM")
+  
+  # save the normalized count table
+  count.table.norm <- cpm(y)
+  write.csv(count.table.norm, paste(control, treat, 'norm.csv', sep = '_'))
+  
+  # Do DEA !!!
+  # define the group
+  subject <- factor(c(seq(1, num.sample.control), seq(1, num.sample.treat)))
+  groups <- factor(c(rep("C", num.sample.control), rep("T", num.sample.treat)))
+  
+  y$samples$subject <- subject
+  y$samples$group <- groups
+  
+  # The design matrix
+  if (pair.test) {
+    design <- model.matrix(~subject+groups)
+  }
+  else {
+    design <- model.matrix(~groups)
+  }
+  rownames(design) <- colnames(y)
+  
+  # Estimating the dispersion
+  
+  # estimate the NB dispersion for the dataset
+  y <- estimateDisp(y, design, robust = TRUE)
+  
+  # Differential expression
+  
+  # determine differentially expressed genes
+  # fit genewise glms
+  fit <- glmFit(y, design)
+  
+  # conduct likelihood ratio tests for tumour vs normal tissue differences and show the top genes
+  lrt <- glmLRT(fit)
+  
+  # the DEA result for all the genes
+  # dea <- lrt$table
+  toptag <- topTags(lrt, n = nrow(y$genes), p.value = 1)
+  dea <- toptag$table  # just to add one more column of FDR
+  
+  # differentially expressed genes
+  toptag <- topTags(lrt, n = nrow(y$genes), p.value = 0.05)
+  deg <- toptag$table
+  
+  # save the DEA result and DEGs to files
+  write.csv(dea, paste('dea_', control, '_', treat, '.csv', sep = ''), row.names = F)
+  write.csv(deg, paste('deg_', control, '_', treat, '.csv', sep = ''), row.names = F) 
+}
+
+# the main function
+for (ith.comparison in c(1:num.comparison)) {
+  control <- controls[ith.comparison]
+  treat <- treats[ith.comparison]
+  DEA(control, treat)
+}
