c9332ef7326fb42cbcfa74fcbb08ccdc309ca489
zhxiaokang <zhxiaokang@gmail.com>
2019-09-28 15:59:40 -0700

    rescale figure for rule visualize

--- scripts/visualize.R
+++ scripts/visualize.R
@@ -1,9 +1,12 @@
 # load the libraries
+if (!require("plotscale")) install.packages("ploscale")
+
 library(yaml)
 library(mygene)
 library(EnhancedVolcano)
 library(RColorBrewer)
 library(data.table)
+library(plotscale)
 
 # ====================== load parameters in config file ======================
 
@@ -39,9 +42,9 @@ for (i in c(1:length(gene.dea))) {
 }
 
 # volcano plot
-pdf(file = paste(output.path, '/volcano_plot.pdf', sep = ''), width = 6, height = 3.5)
-EnhancedVolcano(dea.table, lab = gene.dea, xlab = bquote(~Log[2]~ "fold change"), x = 'logFC', y = 'FDR', pCutoff = 10e-5,
-                FCcutoff = 1, xlim = c(-5, 5), ylim = c(0, 10), transcriptPointSize = 1.5)
+fig.volcano <- EnhancedVolcano(dea.table, lab = gene.dea, xlab = bquote(~Log[2]~ "fold change"), x = 'logFC', y = 'FDR', pCutoff = 10e-5,
+                               FCcutoff = 1, xlim = c(-5, 5), ylim = c(0, 10), transcriptPointSize = 1.5)
+as.pdf(fig.volcano, width = 8, height = 5, scaled = TRUE, file = paste(output.path, '/volcano_plot.pdf', sep = ''))
 dev.off()
 
 # heatmap
@@ -53,14 +56,35 @@ norm.table.temp <- as.data.frame(norm.table.temp)
 norm.table <- norm.table.temp[, -1]
 row.names(norm.table) <- unlist(norm.table.temp[, 1])
 
-index.deg <- which(row.names(norm.table) %in% gene.id.deg)
+# instead using all genes, only use the top 50 degs if there are more than 50
+if (length(gene.id.deg) > 50) {
+  index.deg <- which(row.names(norm.table) %in% gene.id.deg[1:50])
+} else {
+  index.deg <- which(row.names(norm.table) %in% gene.id.deg)
+}
 norm.table.deg <- norm.table[index.deg,]
+
+gene.id.norm.table <- rownames(norm.table.deg)
+
+gene.symbol.norm.table <- queryMany(gene.id.norm.table, scopes = 'ensembl.gene', fields = 'symbol')$symbol
+
+# if can't find a symbol for the id, then keep the id as it is
+gene.norm.table <- gene.symbol.norm.table
+for (i in c(1:length(gene.norm.table))) {
+  if (is.na(gene.norm.table[i])) {
+    gene.norm.table[i] <- gene.id.norm.table[i]
+  }
+}
+
+# replace the rownames
+rownames(norm.table.deg) <- gene.norm.table
+
 palette <- brewer.pal(n = length(levels(group.all)), name = "Set1")
 palette.group <- group.all
 for (i in c(1:length(levels(palette.group)))) {
   levels(palette.group)[levels(palette.group) == levels(palette.group)[i]] <- palette[i]
 }
 ## draw heatmap
-pdf(file = paste(output.path, '/heatmap.pdf', sep = ''), width = 6, height = 3.5)
-heatmap(as.matrix(norm.table.deg), ColSideColors = as.character(palette.group))
+pdf(file = paste(output.path, '/heatmap.pdf', sep = ''), width = 15, height = 12)
+heatmap(as.matrix(norm.table.deg), ColSideColors = as.character(palette.group), margins = c(8,5))
 dev.off()
